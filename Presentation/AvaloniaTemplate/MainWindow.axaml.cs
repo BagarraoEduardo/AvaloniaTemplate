2ce99f0644646a05d63cca862d0183eae5581958
Eduardo Bagarrão <general@eduardobagarrao.com>
2025-02-23 21:21:38 +0000

    Template setup

---
+++ Presentation/AvaloniaTemplate/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AvaloniaTemplate;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Source: https://medium.com/@mohanedzekry/clean-architecture-in-asp-net-core-web-api-d44e33893e1d
